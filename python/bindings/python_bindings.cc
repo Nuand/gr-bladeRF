7c1d3b5fddf7f4c191e189b72e59a15e4d81cf59
Sergey Musikhin <musihin_sergei@mail.ru>
2021-05-18 00:10:54 +0300

    source appears in gnuradio

--- python/bindings/python_bindings.cc
+++ python/bindings/python_bindings.cc
@@ -21,6 +21,7 @@ namespace py = pybind11;
 // Please do not delete
 /**************************************/
 // BINDING_FUNCTION_PROTOTYPES(
+    void bind_source(py::module& m);
 // ) END BINDING_FUNCTION_PROTOTYPES
 
 
@@ -49,5 +50,6 @@ PYBIND11_MODULE(bladeRF_python, m)
     // Please do not delete
     /**************************************/
     // BINDING_FUNCTION_CALLS(
+    bind_source(m);
     // ) END BINDING_FUNCTION_CALLS
 }
