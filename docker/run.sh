fadaad09ce922dd577cf6ed18ca4650048aebe03
Robert Ghilduta <robert.ghilduta@nuand.com>
2022-09-03 02:54:45 -0700

    Update docker

--- docker/run.sh
+++ docker/run.sh
@@ -7,12 +7,13 @@ xhost +
 
 sudo docker run \
   -it --rm --privileged \
+  --net=host \
   -e PULSE_SERVER=unix:/run/user/1000/pulse/native \
   -v /var/lib/dbus:/var/lib/dbus \
   -v /dev/bus/usb:/dev/bus/usb \
   -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-  -e DISPLAY=unix$DISPLAY \
+  -v $HOME/.Xauthority:/home/blade/.Xauthority \
+  -e DISPLAY \
   -v /run/user/1000/pulse:/run/user/1000/pulse \
-  -v $(pwd):/home/blade/gr-bladeRF \
   -u blade \
-  smusihin/blade-dev /bin/bash
+  gnuradio /bin/bash
