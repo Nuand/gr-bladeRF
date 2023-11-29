27de2898dbee75d55c61f541315e3853e602e526
Robert Ghilduta <robert.ghilduta@nuand.com>
2023-11-29 04:16:45 -0800

    keep track of channel initializations in an ordered set

--- lib/channel_store.h
+++ lib/channel_store.h
@@ -1,21 +1,30 @@
 #include <map>
+#include <set>
 
 template<typename T>
 class channel_store
 {
   std::map<size_t, T> values_;
+  std::set<size_t> initialized_channels_;
+
 public:
   template <typename pred>
   T set_if_not_equal(T value, size_t n, size_t max_n, pred fn)
   {
       if( n < max_n)
       {
-          auto &val = values_[n];
-          if(val != value)
-          {
-              val = fn();
-          }
-          return val;
+         if( initialized_channels_.find(n) == initialized_channels_.end())
+         {
+             values_[n] = fn();
+             initialized_channels_.insert(n);
+         } else {
+             auto &val = values_[n];
+             if( val != value)
+             {
+                 val = fn();
+             }
+         }
+         return values_[n];
       }
       return T{};
   }
